ef13360fd4a49c71051401d7d1eb0879f7c1e890
RyanMcDougall <ryan.mcdougall@target.com>
2018-05-10 08:12:49 -0500

    fixed web dependency issue

--- src/main/groovy/com/phatlabs/ninja/groovyboot/entities/Note.groovy
+++ src/main/groovy/com/phatlabs/ninja/groovyboot/entities/Note.groovy
@@ -2,13 +2,16 @@ package com.phatlabs.ninja.groovyboot.entities
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import groovy.transform.Canonical
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+
 import javax.persistence.*
 import javax.validation.constraints.*
 
 @Entity
 @Table(name="notes")
 @Canonical
-//@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters= true)
+@JsonIgnoreProperties(value = ["createdAt", "updatedAt"], allowGetters= true)
 class Note {
 
     @Id
@@ -20,5 +23,15 @@ class Note {
 
     @NotEmpty
     String content
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt
 
 }
